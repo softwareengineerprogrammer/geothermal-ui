d8f77193701b2df831050666dfff7915a06d31da
softwareengineerprogrammer <4056124+softwareengineerprogrammer@users.noreply.github.com>
2023-10-21 14:26:10 -0700

    Basically-working form submission

--- hip-ra/hip-ra.js
+++ hip-ra/hip-ra.js
@@ -1,18 +1,79 @@
-import { createApp, ref } from 'vue'
+import {createApp, ref} from 'vue'
 
-function getLocationHost(){
+
+function getLocationHost() {
     return location.hostname
 }
 
-function getLocationOrigin(){
+function getLocationOrigin() {
     return location.origin
 }
+
+function submitHipRa() {
+    console.log('HIP RA submitted')
+}
+
+
 
 createApp({
     data() {
         return {
             locationHost: getLocationHost(),
-            locationOrigin: getLocationOrigin()
+            locationOrigin: getLocationOrigin(),
+            hipRaInputText: ref(`Reservoir Temperature, 250.0
+Rejection Temperature, 60.0
+Formation Porosity, 10.0
+Reservoir Area, 55.0
+Reservoir Thickness, 0.25
+Reservoir Life Cycle, 25
+Heat Capacity Of Water, -1
+Density Of Water, -1`),
+            hipRaResult: ref('Run HIP RA to get a result'),
+            errorMessage: ref('')
+        }
+    },
+    methods: {
+        submitHipRa() {
+            /*
+            action="https://d4nshmdoig.execute-api.us-west-2.amazonaws.com/get-hip-ra-result"
+            method="POST"
+            */
+            this.errorMessage = ''
+            this.hipRaResult = ''
+
+            console.log('HIP RA submitted')
+            console.log('HIP RA input text is:', this.hipRaInputText)
+
+            let params = {}
+            try {
+                this.hipRaInputText.split('\n').forEach(it => {
+                    let kv = it.trim().split(',')
+                    let name = kv[0].trim()
+                    let value = kv[1].trim()
+                    params[name] = value
+                })
+                console.log('Parsed params as:',params)
+            } catch(e) {
+                console.log('Error parsing params:',e)
+                this.errorMessage = "Failed to parse parameters"
+                return false
+            }
+
+            fetch(
+                'https://d4nshmdoig.execute-api.us-west-2.amazonaws.com/get-hip-ra-result',
+                {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        hip_ra_input_parameters: params
+                    })
+                }
+            ).then(response => {
+                    console.log('Response:',response)
+                    response.json().then( responseJson => {
+                        console.log('Response body:\n',responseJson)
+                        this.hipRaResult = responseJson
+                    })
+                })
         }
     }
 }).mount('#app')
